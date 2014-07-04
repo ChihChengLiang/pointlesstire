7b813d7d4162b18661cbd4926eac83b152f086cd
SatoshiLiang <satoshiliang@devtable.com>
2014-07-04 03:48:00 +0000

    FizzBuzz done

--- pointlesstire.py
+++ pointlesstire.py
@@ -4,6 +4,7 @@ from google.appengine.ext.webapp import template
 import cgi
 import string
 import re
+from jinja2 import Environment, FileSystemLoader
 
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
@@ -82,9 +83,20 @@ class Welcome(webapp2.RequestHandler):
   def get(self):
     self.response.out.write("<h1>Welcom,"+self.request.get("username")+"!</h1>")
 
+class FizzBuzz(webapp2.RequestHandler):
+  def get(self):
+    n=self.request.get("n")
+    n= n and int(n)
+    jinja_env= Environment(loader=FileSystemLoader('templates'))
+    template = jinja_env.get_template('FizzBuzz.html')
+    self.response.out.write(template.render(n=n))
+    #self.response.out.write("<h1>Python code work fine!n=%d</h1>" % n)
+
+
 app = webapp2.WSGIApplication([
   ('/', MainHandler),
   ('/ROT13',ROT13),
   ('/signup',SignUp),
-  ('/welcome',Welcome)
+  ('/welcome',Welcome),
+  ('/FizzBuzz',FizzBuzz)
 ], debug=True)
