f119c89498c19a77fde8c1cd64e96b231a7296db
SatoshiLiang <chihchengliang@gmail.com>
2014-09-17 11:45:20 +0800

    it's a massive python tidy bombard

--- pointlesstire.py
+++ pointlesstire.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import webapp2
 import os
 from google.appengine.ext.webapp import template
@@ -8,145 +11,182 @@ from jinja2 import Environment, FileSystemLoader
 from google.appengine.ext import db
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-PWD_RE  = re.compile(r"^.{3,20}$")
-EML_RE  = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+PWD_RE = re.compile(r"^.{3,20}$")
+EML_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
 
-alphabet_list=list(string.lowercase)
+alphabet_list = list(string.lowercase)
+
 
 class BaseHandler(webapp2.RequestHandler):
-  
-  def write(self, filename, **template_values):
-    path = os.path.join(os.path.dirname(__file__), filename)
-    self.response.out.write(template.render(path, template_values))
-    
-  def render(self, filename,**template_values):
-    jinja_env= Environment(loader=FileSystemLoader('templates'))
-    template = jinja_env.get_template(filename)
-    self.response.out.write(template.render(template_values))
-  
-class MainHandler(webapp2.RequestHandler):
-  def get(self):
-    template_values = {
-      'name': "World",
-    }
 
-    path = os.path.join(os.path.dirname(__file__), 'index.html')
-    self.response.out.write(template.render(path, template_values))
+    def write(self, filename, **template_values):
+        path = os.path.join(os.path.dirname(__file__), filename)
+        self.response.out.write(template.render(path, template_values))
+
+    def render(self, filename, **template_values):
+        jinja_env = Environment(loader=FileSystemLoader('templates'))
+        template = jinja_env.get_template(filename)
+        self.response.out.write(template.render(template_values))
+
+
+class MainHandler(webapp2.RequestHandler):
+
+    def get(self):
+        template_values = {'name': 'World'}
+
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
 
 class ROT13(webapp2.RequestHandler):
-  def get(self):
-    template_values = {
-      "default_text":"",
-      }
-    path = os.path.join(os.path.dirname(__file__), 'ROT13.html')
-    self.response.out.write(template.render(path, template_values))
-  def post(self):
-    text=self.request.get("text")
-    output=""
-    for t in text:
-      if t.isalpha():
-        s=alphabet_list[(alphabet_list.index(t.lower())+13) %26]
-        if t.isupper(): 
-          s=s.upper()
-        output+=s
-      else:
-        output+=t
-    template_values = {
-      "default_text":cgi.escape(output,quote=True),
-      }
-    path = os.path.join(os.path.dirname(__file__), 'ROT13.html')
-    self.response.out.write(template.render(path, template_values))
+
+    def get(self):
+        template_values = {'default_text': ''}
+        path = os.path.join(os.path.dirname(__file__), 'ROT13.html')
+        self.response.out.write(template.render(path, template_values))
+
+    def post(self):
+        text = self.request.get('text')
+        output = ''
+        for t in text:
+            if t.isalpha():
+                s = alphabet_list[(alphabet_list.index(t.lower()) + 13)
+                                  % 26]
+                if t.isupper():
+                    s = s.upper()
+                output += s
+            else:
+                output += t
+        template_values = {'default_text': cgi.escape(output,
+                           quote=True)}
+        path = os.path.join(os.path.dirname(__file__), 'ROT13.html')
+        self.response.out.write(template.render(path, template_values))
+
 
 class SignUp(webapp2.RequestHandler):
-  def get(self):
-    template_values = {
-      "user_error":"",
-      "pwd_error":"",
-      "vrypwd_error":"",
-      "email_error":""
-      }
-    path = os.path.join(os.path.dirname(__file__), 'signup.html')
-    self.response.out.write(template.render(path, template_values))
-  def post(self):
-    username=self.request.get("username")
-    
-    user_correct  = USER_RE.match(username)
-    pwd_correct   = PWD_RE.match(self.request.get("password"))
-    vrypwd_correct= self.request.get("password")==self.request.get("verify")
-    email_correct = EML_RE.match(self.request.get("email"))
-    error_msg={
-      "user_error":"That's not a valid username.",
-      "pwd_error":"That wasn't a valid password.",
-      "vrypwd_error":"Your passwords didn't match.",
-      "email_error":"That's not a valid email."
-      }
-    if user_correct and pwd_correct and vrypwd_correct and email_correct:
-      self.redirect("/welcome?username="+cgi.escape(username,quote=True))
-    else:
-      if user_correct: error_msg["user_error"]=""
-      if pwd_correct: error_msg["pwd_error"]=""
-      if vrypwd_correct: error_msg["vrypwd_error"]=""
-      if email_correct: error_msg["email_error"]=""
-      path = os.path.join(os.path.dirname(__file__), 'signup.html')
-      self.response.out.write(template.render(path, error_msg))
-    pass
+
+    def get(self):
+        template_values = {
+            'user_error': '',
+            'pwd_error': '',
+            'vrypwd_error': '',
+            'email_error': '',
+            }
+        path = os.path.join(os.path.dirname(__file__), 'signup.html')
+        self.response.out.write(template.render(path, template_values))
+
+    def post(self):
+        username = self.request.get('username')
+
+        user_correct = USER_RE.match(username)
+        pwd_correct = PWD_RE.match(self.request.get('password'))
+        vrypwd_correct = self.request.get('password') \
+            == self.request.get('verify')
+        email_correct = EML_RE.match(self.request.get('email'))
+        error_msg = {
+            'user_error': "That's not a valid username.",
+            'pwd_error': "That wasn't a valid password.",
+            'vrypwd_error': "Your passwords didn't match.",
+            'email_error': "That's not a valid email.",
+            }
+        if user_correct and pwd_correct and vrypwd_correct \
+            and email_correct:
+
+            self.redirect('/welcome?username=' + cgi.escape(username,
+                          quote=True))
+        else:
+            if user_correct:
+                error_msg['user_error'] = ''
+            if pwd_correct:
+                error_msg['pwd_error'] = ''
+            if vrypwd_correct:
+                error_msg['vrypwd_error'] = ''
+            if email_correct:
+                error_msg['email_error'] = ''
+            path = os.path.join(os.path.dirname(__file__), 'signup.html'
+                                )
+            self.response.out.write(template.render(path, error_msg))
+        pass
+
+
 class Welcome(webapp2.RequestHandler):
-  def get(self):
-    self.response.out.write("<h1>Welcom,"+self.request.get("username")+"!</h1>")
+
+    def get(self):
+        self.response.out.write('<h1>Welcom,'
+                                + self.request.get('username')
+                                + '!</h1>')
+
 
 class FizzBuzz(webapp2.RequestHandler):
-  def get(self):
-    n=self.request.get("n")
-    n= n and int(n)
-    jinja_env= Environment(loader=FileSystemLoader('templates'))
-    template = jinja_env.get_template('FizzBuzz.html')
-    self.response.out.write(template.render(n=n))
-    #self.response.out.write("<h1>Python code work fine!n=%d</h1>" % n)
+
+    def get(self):
+        n = self.request.get('n')
+        n = n and int(n)
+        jinja_env = Environment(loader=FileSystemLoader('templates'))
+        template = jinja_env.get_template('FizzBuzz.html')
+        self.response.out.write(template.render(n=n))
+
+
+    # self.response.out.write("<h1>Python code work fine!n=%d</h1>" % n)
 
 ## The blog #########################
 
-def blog_key(name="default"):
-  return db.Key.from_path('blogs',name)
+def blog_key(name='default'):
+    return db.Key.from_path('blogs', name)
+
 
 class BlogModel(db.Model):
-  subject = db.StringProperty(required=True)
-  content = db.TextProperty(required=True)
-  created = db.DateTimeProperty(auto_now_add=True)
-  
+
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
 class Blog(BaseHandler):
-  def get(self):
-    posts = db.GqlQuery("select * from BlogModel order by created desc limit 10")
-    self.render('blog_main.html',posts = posts)
-    
+
+    def get(self):
+        posts = \
+            db.GqlQuery('select * from BlogModel order by created desc limit 10'
+                        )
+        self.render('blog_main.html', posts=posts)
+
+
 class BlogNewPost(BaseHandler):
-  def get(self):
-    self.render('blog_newpost.html')
-  def post(self):
-    subject = self.request.get("subject")
-    content = self.request.get("content")
-    if subject and content:
-      a = BlogModel(parent= blog_key(), subject=subject, content=content) 
-      a.put()
-      self.redirect("/blog/%s" % str(a.key().id()))
-    else:
-      self.render('blog_newpost.html',error=True)
+
+    def get(self):
+        self.render('blog_newpost.html')
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+        if subject and content:
+            a = BlogModel(parent=blog_key(), subject=subject,
+                          content=content)
+            a.put()
+            self.redirect('/blog/%s' % str(a.key().id()))
+        else:
+            self.render('blog_newpost.html', error=True)
+
 
 class PostPage(BaseHandler):
-  def get(self,post_id):
-    key = db.Key.from_path('BlogModel',int(post_id), parent=blog_key())
-    post= db.get(key)
-    if not post:
-      self.render("custom404.html")
-      return
-    self.render("blog_permalink.html", post=post)
+
+    def get(self, post_id):
+        key = db.Key.from_path('BlogModel', int(post_id),
+                               parent=blog_key())
+        post = db.get(key)
+        if not post:
+            self.render('custom404.html')
+            return
+        self.render('blog_permalink.html', post=post)
+
 
 app = webapp2.WSGIApplication([
-  ('/', MainHandler),
-  ('/ROT13',ROT13),
-  ('/signup',SignUp),
-  ('/welcome',Welcome),
-  ('/FizzBuzz',FizzBuzz),
-  ('/blog/?',Blog),
-  ('/blog/newpost',BlogNewPost),
-  ('/blog/(\d+)',PostPage)
-], debug=True)
+    ('/', MainHandler),
+    ('/ROT13', ROT13),
+    ('/signup', SignUp),
+    ('/welcome', Welcome),
+    ('/FizzBuzz', FizzBuzz),
+    ('/blog/?', Blog),
+    ('/blog/newpost', BlogNewPost),
+    ('/blog/(\d+)', PostPage),
+    ], debug=True)
